c3632c400bc2953a1d369699ac6adcd0c7d5964f
David-M-Nielsen <monbergnielsen@outlook.com>
2019-04-13 00:16:25 +0200

    added basic arguements. Still missing some, but this is how it will go forwards for now

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Super-Password-Manager
 A simple password manager for linux systems. NOTE: USE AT OWN RISK.
+
+# TODO
+* Add flag parsing
--- spasm.sh
+++ spasm.sh
@@ -1,3 +1,29 @@
-#!/bin/sh
+#/bin/sh
+
+ROOT="$home/.local/spasm/"
+optstring="hG:p:"
+
+usage(){ printf "usage: spasm [-h] [-G length | -p password]"; }
+
+gen_pass() {
+	< /dev/urandom tr -dc A-Za-z0-9 | head -c$1; echo # Temporary. Overhaul to include special characters and follow OWASP
+}
 
 
+
+while getopts $optstring arg; do
+	case ${arg} in
+		
+		# Usable args
+		h)	usage ;;
+		G)	[[ -n "$PASS" ]] && usage || PASS=$(gen_pass $OPTARG) ;;
+		p)	[[ -n "$PASS" ]] && usage || PASS=${OPTARG} ;;
+	
+		# Error handling
+		\?)	printf "invalid argument -- $OPTARG" 1>&2; usage ;;
+		:)	printf "argument needed to -$OPTARG" 1>&2; usage ;;
+		*) usage ;;
+	esac
+done
+
+echo $PASS

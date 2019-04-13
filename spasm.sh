2de0b4e1b14e67936928b0ddaf3224a063cb0516
David-M-Nielsen <monbergnielsen@outlook.com>
2019-04-14 01:50:20 +0200

    refactored a few things

--- spasm.sh
+++ spasm.sh
@@ -1,29 +1,38 @@
-#/bin/sh
+#/bin/sh -ex
 
-ROOT="$home/.local/spasm/"
-optstring="hG:p:"
+ROOT="~/.local/share/spasm/"
+pass_cat=""
+pass=""
 
-usage(){ printf "usage: spasm [-h] [-G length | -p password]"; }
+optstring="hG:p:a:"
+
+usage(){ 
+	echo "usage: spasm [-h] [-G length | -p password]"
+}
 
 gen_pass() {
 	< /dev/urandom tr -dc A-Za-z0-9 | head -c$1; echo # Temporary. Overhaul to include special characters and follow OWASP
 }
 
+# test if category exists
+schrodinger(){
+	local cat=$(echo $1 | cut -d"/" -f1)
+	local dir="$ROOT$cat"
+	echo $dir
+	mkdir -p $dir
+}
 
-
-while getopts $optstring arg; do
-	case ${arg} in
-		
-		# Usable args
-		h)	usage ;;
-		G)	[[ -n "$PASS" ]] && usage || PASS=$(gen_pass $OPTARG) ;;
-		p)	[[ -n "$PASS" ]] && usage || PASS=${OPTARG} ;;
-	
+while getopts $optstring FLAG; 
+do	
+	case $FLAG in	
+		h) usage; exit 0 ;; # prints help
+		G) [[ -n "$pass" ]] && usage || pass=$(gen_pass $OPTARG) ;; #generates password
+		p) [[ -n "$pass" ]] && usage || pass=${OPTARG} ;; #takes password from user
+		a) schrodinger $OPTARG ;;
 		# Error handling
-		\?)	printf "invalid argument -- $OPTARG" 1>&2; usage ;;
-		:)	printf "argument needed to -$OPTARG" 1>&2; usage ;;
-		*) usage ;;
+		\?) usage; exit 1 ;;
 	esac
 done
 
-echo $PASS
+#temporary output for test purposes
+#echo $PASS

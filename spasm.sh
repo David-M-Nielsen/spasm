be1a09d98ed0be4a1e44c9a88a65618928ef3cf4
David-M-Nielsen <monbergnielsen@outlook.com>
2019-04-20 01:43:07 +0200

    added xclip support for direct-to-clipboard. added option to print password to terminal

--- spasm.sh
+++ spasm.sh
@@ -1,9 +1,11 @@
 #/bin/sh -ex
 #TODO reorganize whole file
+#TODO add function to print database structure
 DATA="$HOME/.local/share/spasm" #TODO look into a way of improving this to fit more systems. XDG_DATA_HOME maybe?
 
 OPT_ADD=
 OPT_GET=
+PRINT_PASS=
 
 cat=""
 name=""
@@ -12,7 +14,7 @@ pass=""
 #TODO include all program options
 #TODO improve formatting and readability
 usage(){ 
-	echo "usage: spasm [ -h ] [ -G length | -p password ] [ -a [category]/[name] ] [ -g [category]/[name] ] [ -d [category/name] ]"
+	echo "usage: spasm [ -h ] [ -P] [ -G length | -p password ] [ -a [category]/[name] ] [ -g [category]/[name] ] [ -d [category/name] ]"
 }
 
 explode(){
@@ -44,7 +46,13 @@ add_pass(){
 
 #TODO implement decrypting password file
 get_pass(){
-	[ ! -f "$DATA/$cat/$name" ] && echo "invalid argument -- no such category or password" && explode || cat "$DATA/$cat/$name" 
+	[ ! -f "$DATA/$cat/$name" ] && echo "invalid argument -- no such category or password" && explode
+	
+	pass=$(echo $(<"$DATA/$cat/$name"))
+
+	type "xclip" >/dev/null 2>&1 && echo $pass | xclip -selection c || PRINT_PASS=true
+
+	[ $PRINT_PASS ] && echo $pass
 }
 
 # parses if the path was typed correctly: "category/service"
@@ -66,7 +74,7 @@ delete_pass(){
 	[ ! "$(ls -A "$DATA/$cat")" ] && rmdir "$DATA/$cat"
 }
 
-optstring="hG:p:a:g:d:"
+optstring="hG:p:a:g:d:P"
 while getopts $optstring FLAG; #TODO add handling for no options passed
 do	
 	case $FLAG in	
@@ -74,7 +82,8 @@ do
 	G) [ -n "$pass" ] && explode || pass=$(gen_pass $OPTARG) ;;#TODO add proper error messages
 	p) [ -n "$pass" ] && explode || pass=${OPTARG} ;;#TODO add proper error messages
 	a) parse_path $OPTARG && OPT_ADD=true ;;
-	g) parse_path $OPTARG && OPT_GET=true ;; 
+	g) parse_path $OPTARG && OPT_GET=true ;;
+	P) PRINT_PASS=true ;;
 	d) parse_path $OPTARG && delete_pass $OPTARG ;;
 
 	\?) usage; exit 1 ;;
